cae58bce4a4e798368c46bfefd61e7bd657db07a
Connor O'Brien <connor@hifranc.com>
2020-12-14 13:17:06 +0200

    Add answers

---
+++ js/script.js
@@ -0,0 +1,117 @@
+// question 1
+var firstNumber = 34;
+var secondNumber = 89;
+
+if (firstNumber < secondNumber) {
+    console.log("The first number is smaller than the second");
+}
+
+// question 2
+var age = 11;
+var minimumAge = "13";
+
+if (age >= parseInt(minimumAge)) {
+    console.log("Minimum age met");
+} else {
+    console.log("Minimum age not met");
+}
+
+// question 3
+var income = "11.050";
+var maximumIncome = "13.075";
+
+if (parseFloat(income) <= parseFloat(maximumIncome)) {
+    console.log("Maximum income requirement met");
+} else {
+    console.log("Maximum income requirement not met");
+}
+
+// question 4
+var studentAge = 8;
+
+var message;
+
+if (studentAge >= 6 && studentAge <= 13) {
+    message = "Barneskole";
+} else if (studentAge >= 14 && studentAge <= 16) {
+    message = "Ungdomsskole";
+} else if (studentAge >= 17 && studentAge <= 19) {
+    message = "Videregående skole";
+} else {
+    message = "Invalid age";
+}
+
+console.log(message);
+
+// question 5
+var colour = "blue";
+
+if (colour === "orange") {
+    console.log("This colour is a bit rubbish");
+}
+
+// question 6
+var invoicePaid = false;
+
+if (invoicePaid === false) {
+    console.log("Not paid");
+}
+
+// or
+if (invoicePaid !== true) {
+    console.log("Not paid");
+}
+
+// or
+if (!invoicePaid) {
+    console.log("Not paid");
+}
+
+// question 7
+var selectedNumber = 11;
+var winningNumber = "15";
+
+if (selectedNumber !== parseInt(winningNumber)) {
+    console.log("You lose again");
+}
+
+// question 8
+var dayOfTheWeek = 2;
+
+var dayName;
+
+switch (dayOfTheWeek) {
+    case 1:
+        dayName = "Monday";
+        break;
+
+    case 2:
+        dayName = "Tuesday";
+        break;
+
+    case 3:
+        dayName = "Wednesday";
+        break;
+
+    case 4:
+        dayName = "Thursday";
+        break;
+
+    case 5:
+        dayName = "Friday";
+        break;
+
+    case 6:
+        dayName = "Saturday";
+        break;
+
+    case 7:
+        dayName = "Sunday";
+        break;
+
+    default:
+        dayName = "Invalid day number";
+        break;
+}
+
+console.log(dayName);
